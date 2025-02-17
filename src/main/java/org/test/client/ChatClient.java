c2372532f9b0620befe9ec5ed42aa6115ea4997b
dariogguillen <guillendario@gmail.com>
2025-02-17 15:50:44 -0600

    feat: basic chat client implementation

---
+++ src/main/java/org/test/client/ChatClient.java
@@ -0,0 +1,43 @@
+package org.test.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ChatClient {
+    private Socket socket = null;
+    private BufferedReader inputConsole = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+
+    public ChatClient(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected to the server: " + address + ":" + port);
+            inputConsole = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = "";
+            while(!line.equals("exit")) {
+                line = inputConsole.readLine();
+                out.println(line);
+                System.out.println("Server response: " + in.readLine());
+            }
+
+            socket.close();
+            inputConsole.close();
+            out.close();
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient("127.0.0.1", 5000);
+    }
+}
